d07db52fcaf22d20d9086e8153be1a5aa33bcb7f
Li Yuan <202070317@163.com>
2011-10-18 11:06:41 +0800

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 DemoApp::Application.routes.draw do
   
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy] 
+
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'  
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
@@ -10,7 +15,7 @@ DemoApp::Application.routes.draw do
 
   resources :microposts
 
-  resources :users
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
